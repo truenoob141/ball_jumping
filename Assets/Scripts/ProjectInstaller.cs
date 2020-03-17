32bfed3c2f85cb0c15d871ea67e4664adfdf13a6
TrueNoob141 <truenoob141@gmail.com>
2020-03-17 18:17:03 +0300

    Created SceneManager

---
+++ Assets/Scripts/ProjectInstaller.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Zenject;
+
+namespace Project
+{
+    public class ProjectInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            Container.BindInterfacesAndSelfTo<SceneManager>().AsSingle();
+        }
+    }
+}

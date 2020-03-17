c12583f66ec1f8a36e83661e572c2b37f7119fca
TrueNoob141 <truenoob141@gmail.com>
2020-03-17 18:19:44 +0300

    Created GameSettings

--- Assets/Scripts/ProjectInstaller.cs
+++ Assets/Scripts/ProjectInstaller.cs
@@ -6,8 +6,24 @@ using Zenject;
 
 namespace Project
 {
+    [System.Serializable]
+    public class PlanetSettings
+    {
+        public string id;
+        public float gravity;
+        public Color skyColor;
+    }
+
     public class ProjectInstaller : MonoInstaller
     {
+        [System.Serializable]
+        public class Settings
+        {
+            public List<PlanetSettings> planets;
+        }
+
+        [Inject] private Settings settings;
+
         public override void InstallBindings()
         {
             Container.BindInterfacesAndSelfTo<SceneManager>().AsSingle();

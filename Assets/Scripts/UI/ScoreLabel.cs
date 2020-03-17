08df9b8e9e9b338c6624a2784ccc3d2754eb73ae
TrueNoob141 <truenoob141@gmail.com>
2020-03-17 19:06:12 +0300

    Created ScoreLabel

---
+++ Assets/Scripts/UI/ScoreLabel.cs
@@ -0,0 +1,27 @@
+﻿using TMPro;
+using UnityEngine;
+using Zenject;
+
+namespace Project
+{
+    [RequireComponent(typeof(TMP_Text))]
+    public class ScoreLabel : MonoBehaviour
+    {
+        [SerializeField]
+        private string format = "Score : {0}";
+
+        [Inject] private Play.GameManager gameManager;
+
+        private TMP_Text label;
+
+        private void Awake()
+        {
+            this.label = GetComponent<TMP_Text>();
+        }
+
+        private void OnEnable()
+        {
+            this.label.text = string.Format(this.format, gameManager.Score);
+        }
+    }
+}

9ff3d5193c66905a32878628afdec6727cd60317
TrueNoob141 <truenoob141@gmail.com>
2020-03-17 18:53:17 +0300

    Created GameSky

---
+++ Assets/Scripts/Play/GameSky.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+namespace Project.Play
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class GameSky : MonoBehaviour
+    {
+        [Inject] private GameManager gameManager;
+        [Inject] private SignalBus signalBus;
+
+        private SpriteRenderer spriteRenderer;
+
+        private void Awake()
+        {
+            this.spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        private void OnEnable()
+        {
+            this.signalBus.Subscribe<OnGameStart>(OnGameStart);
+            OnGameStart();
+        }
+
+        private void OnDisable()
+        {
+            this.signalBus.Unsubscribe<OnGameStart>(OnGameStart);
+        }
+
+        private void OnGameStart()
+        {
+            if (!this.gameManager.IsValidGame)
+                return;
+
+            this.spriteRenderer.color = gameManager.CurrentPlanet.skyColor;
+        }
+    }
+}
